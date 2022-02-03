d806ce5f20bb3825e58f56d3293c1867db7848bd
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-02-03 22:20:03 +0000

    usetate and click events

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import Navbar from './navbar.jsx';
+import Hero from './hero.jsx'
 //import logo from './logo.svg'
 
 
 function App() {
  // const [count, setCount] = useState(0)
+  const [names, setNames] = useState("Mario")
+  const [age , setAge] = useState(15)
+  
+  const handleClick = () => {
+    console.log("I have been clicked")
+  }
 
+  const lion = () => {
+    setNames("Lion")
+    setAge(46)
+  }
+
+   const charles = () => {
+    setNames("Charles")
+    setAge(25)
+   }
+   const ama = () => {
+    setNames("Ama")
+    setAge(36)
+  }
   return (
     <div className="App max-w-screen-lg mx-auto">
       <Navbar />
+      <Hero />
+      <p>{names} is {age} years old. </p>
+      <button onClick={() => lion()} className="px-8 py-3 text-base font-medium bg-gray-900 text-white">Click me</button>
+      <button onClick={() => charles()} className="px-8 py-3 text-base font-medium bg-gray-900 text-white">Click me</button>
+      <button onClick={()=>ama()} className="px-8 py-3 text-base font-medium bg-gray-900 text-white">Click me</button>
     </div>
   )
 }
---
+++ src/hero.jsx
@@ -0,0 +1,9 @@
+const hero = () => {
+    return (
+        <div className="hero py-20">
+            <h1 className="text-5xl lg:text-6xl font-black tracking-normal leading-none text-gray-900">Homepage</h1>
+        </div>
+    )
+}
+
+export default hero;
