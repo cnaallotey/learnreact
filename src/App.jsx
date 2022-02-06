efd871dff011c5711602f9b6eaea36fd332a60ad
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-02-06 14:05:03 +0000

    usefeect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import Navbar from './navbar.jsx';
 import Hero from './hero.jsx'
 import Clicks from './clicks'
 import Lists from './lists'
+
 //import logo from './logo.svg'
 
 
 function App() {
  // const [count, setCount] = useState(0)
+  const [blogs, setBlogs] = useState([
+        { title: "Hello", artiste: "adele", album: "23", id: 1 },
+        { title: "Shape of you", artiste: "Ed Sheeran", album: "Divide", id: 2 },
+        { title: "Fountains", artiste: "Drake", album: "Lover Boy", id: 3 },
+        { title: "Alright", artiste: "Kendrick Lamar", album: "Good Kid Mad City", id: 4 }
+  ]);
+  
+  const deleteBlog = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id != id)
+    setBlogs(newBlogs)
+  }
+  useEffect(() => {
+    console.log("useEffect run")
+  })
   
   
   return (
@@ -15,7 +30,7 @@ function App() {
       <Navbar />
       <Hero />
       <Clicks />
-      <Lists/>
+      <Lists blogs={blogs} deleteBlog = {deleteBlog} />
 
       
     </div>
